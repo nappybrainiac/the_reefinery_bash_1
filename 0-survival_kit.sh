7f877ee124f000e46faa90cc45e4689ee9a67ce9
Gloria Adams <gloriaadams@Glorias-iMac.local>
2016-04-14 15:25:12 -0400

    Reefinery completed

---
+++ 0-survival_kit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+touch unicorn
+echo "are pretty" >> unicorn
+mkdir castle
+mv unicorn castle/
+rm -rf castle/
---
+++ 1-what-you-are-will-show-in-what-you-do.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ls /etc
+cat /etc/passwd
+echo "May the fourth be with you" | awk '{print $4}'
